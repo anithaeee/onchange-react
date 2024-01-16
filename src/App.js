d5c13c4c69888dc61bbfc5117e4b5ef2bbb4da11
Anitha Reddy Pandem <anithaeee224@gmail.com>
2024-01-16 19:37:04 +0800

    xx

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import { InputText } from 'primereact/inputtext';
 import { useState } from 'react';
 function App() {
   const[user, setuser] = useState([]);
+  const handler = e =>{
+    setuser(e.target.value);
+  }
   return (
     <div className="container">
       <div className='text'>
-b         <h1>Onchange-React event handlers</h1>
+        <h1>Onchange-React event handlers</h1>
       </div>
       <div className='input-text'>
-         <InputText></InputText>
-         <Button>Click Me</Button>
+         <input type='text' placeholder='user name' value={user} onChange={handler}></input>
       </div>
     </div>
   );
