70e2b7a0ce7a71ab1ca52e00efe3349f2aa880f6
Anitha Reddy Pandem <anithaeee224@gmail.com>
2024-01-16 19:16:27 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { useState } from 'react';
 function App() {
+  const[user, setuser] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='text'>
+b         <h1>Onchange-React event handlers</h1>
+      </div>
+      <div className='input-text'>
+         <InputText></InputText>
+         <Button>Click Me</Button>
+      </div>
     </div>
   );
 }
